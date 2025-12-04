237cebc003058df716936fb7c9a9dcbd14bf6342
ShadyRaccoon <marianbogdan953@yahoo.com>
2025-12-04 10:42:00 +0200

    init bureau members controller

---
+++ StudentOrg_A4_Website/Controllers/BureauMemberController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentOrg_A4_Website.Data;
+using StudentOrg_A4_Website.Models;
+using StudentOrg_A4_Website.Services;
+using StudentOrg_A4_Website.ViewModels;
+
+namespace StudentOrg_A4_Website.Controllers
+{
+    public class BureauMemberController : Controller
+    {
+    }
+}
