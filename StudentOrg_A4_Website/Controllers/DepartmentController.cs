4bfb829825e58f915521ea49e01a90945f9733a2
ShadyRaccoon <marianbogdan953@yahoo.com>
2025-12-07 15:16:37 +0200

    department index added

---
+++ StudentOrg_A4_Website/Controllers/DepartmentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentOrg_A4_Website.Data;
+
+namespace StudentOrg_A4_Website.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly StudentOrgContext _context;
+
+        public DepartmentController(StudentOrgContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var departmentMembers = await _context.Departments
+                .Include(d => d.Members)
+                .Include(d => d.DepartmentName)
+                .ToListAsync();
+
+            return View(departmentMembers);
+        }
+    }
+}
