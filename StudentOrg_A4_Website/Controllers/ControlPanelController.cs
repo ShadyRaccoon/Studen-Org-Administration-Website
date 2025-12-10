fda9ef85b136a8e68d17117609729c6d0b539378
ShadyRaccoon <marianbogdan953@yahoo.com>
2025-12-10 14:45:17 +0200

    home index controller

---
+++ StudentOrg_A4_Website/Controllers/ControlPanelController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentOrg_A4_Website.Controllers
+{
+    [Authorize(Roles = "Admin,Bureau")]
+    public class ControlPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- StudentOrg_A4_Website/Controllers/HomeController.cs
+++ StudentOrg_A4_Website/Controllers/HomeController.cs
@@ -1,21 +1,30 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using StudentOrg_A4_Website.Data;
 using StudentOrg_A4_Website.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace StudentOrg_A4_Website.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly StudentOrgContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, StudentOrgContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var recentPosts = await _context.Posts
+                .OrderByDescending(p => p.PostDate)
+                .Take(5)
+                .ToListAsync();
+
+            return View(recentPosts);
         }
 
         public IActionResult Privacy()
