5bc43b423f8a4f1bf2d6a6991e0569111050fb03
ShadyRaccoon <marianbogdan953@yahoo.com>
2025-12-10 16:40:09 +0200

    placeholders added

---
+++ StudentOrg_A4_Website/Controllers/PlaceholderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentOrg_A4_Website.Controllers
+{
+    public class PlaceholderController : Controller
+    {
+        public IActionResult ComingSoon()
+        {
+            return View();
+        }
+    }
+}
