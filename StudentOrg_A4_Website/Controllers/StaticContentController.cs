6d96361f47540e342cfae828558ca5a2b4352a2e
ShadyRaccoon <marianbogdan953@yahoo.com>
2025-12-14 21:15:45 +0200

    pagina departamente

---
+++ StudentOrg_A4_Website/Controllers/StaticContentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentOrg_A4_Website.Controllers
+{
+    public class StaticContentController : Controller
+    {
+        public IActionResult Departments()
+        {
+            return View();
+        }
+    }
+}
